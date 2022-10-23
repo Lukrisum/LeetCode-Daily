982456f7b9c86f5e65504374c7efac4b9298cb34
Lukrisum <91905983+Lukrisum@users.noreply.github.com>
2022-10-24 01:21:27 +0800

    20.有效的括号 EASY

---
+++ 20.有效的括号.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=cpp
+ *
+ * [20] 有效的括号
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+  bool isValid(string s)
+  {
+    stack<char> st;
+    for (auto &&c : s)
+    {
+      if ((c == '(') || (c == '[') || (c == '{'))
+      {
+        st.push(c);
+      }
+      else
+      {
+        if (st.empty() || (st.top() == '(' && c != ')') || (st.top() == '[' && c != ']') || (st.top() == '{' && c != '}'))
+        {
+          return false;
+        }
+        else
+        {
+          st.pop();
+        }
+      }
+    }
+    return st.empty();
+  }
+};
+// @lc code=end
