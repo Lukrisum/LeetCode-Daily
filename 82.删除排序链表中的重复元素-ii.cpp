682238e3f51cb9dfbdb30a6cb31313e7d774a7f8
Yeee <2217567783@qq.com>
2022-11-16 23:59:11 +0800

    82.删除排序链表中的重复元素-ii

---
+++ 82.删除排序链表中的重复元素-ii.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=82 lang=cpp
+ *
+ * [82] 删除排序链表中的重复元素 II
+ */
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+  ListNode *deleteDuplicates(ListNode *head)
+  {
+    ListNode *dummy = new ListNode(0);
+    dummy->next = head;
+    ListNode *pre = dummy;
+    ListNode *cur = head;
+    while (cur != nullptr)
+    {
+      while (cur->next && cur->val == cur->next->val)
+      {
+        cur = cur->next;
+      }
+      // cur 前进了
+      if (pre->next != cur)
+      {
+        // 删除重复项
+        pre->next = cur->next;
+
+        // 完成操作，pre 前往下一节点
+      }
+      else
+      {
+        // 无重复项，pre 前往下一节点
+        pre = pre->next;
+      }
+      cur = cur->next;
+    }
+    return dummy->next;
+  }
+};
+
+// @lc code=end
