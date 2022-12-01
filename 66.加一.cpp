c532530d8b8d2a3078858424e46df8c47427b2b6
Lukrisum <91905983+Lukrisum@users.noreply.github.com>
2022-12-01 23:56:53 +0800

    66. 加一

---
+++ 66.加一.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=cpp
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] != 9) {
+                ++digits[i];
+                for (int j = i + 1; j < n; ++j) {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
+
+        // digits 中所有的元素均为 9
+        vector<int> ans(n + 1);
+        ans[0] = 1;
+        return ans;
+    }
+};
+// @lc code=end
+
