96ffd6090b181af6031e1928124375b9d2bbae2f
Lukrisum <91905983+Lukrisum@users.noreply.github.com>
2022-12-06 23:57:40 +0800

    209.长度最小的子数组

--- 209.长度最小的子数组.cpp
+++ 209.长度最小的子数组.cpp
@@ -12,40 +12,35 @@ class Solution
 public:
   int minSubArrayLen(int target, vector<int> &nums)
   {
-
     int minLen = 0;
     int right = 0;
     int sum = 0;
 
-    for (int left = 0; left < nums.size(); left++)
+    for (int left = 0; left < nums.size() - 1; left++)
     {
+      sum += nums[left];
 
-      while (sum < target && right < nums.size())
+      while (sum < target && right < nums.size() - 1)
       {
-        sum += nums[right];
-        right++;
+        if (right == left)
+        {
+          if (right == nums.size() - 1)
+          {
+            return minLen;
+          }
+          right++;
+        }
+        sum += nums[right++];
       }
-      
+
       if (sum >= target)
       {
-        int len = (right - 1) - left + 1;
-        if (minLen == 0)
-        {
-          minLen = len;
-        }
-        else
-        {
-          minLen = min(len, minLen);
-        }
-      }
-      else
-      {
-        return minLen;
+        int tempMin = (right - 1) - left + 1;
+        minLen = (minLen == 0 ? tempMin : min(tempMin, minLen));
       }
 
       sum -= nums[left];
     }
-
     return minLen;
   }
 };
