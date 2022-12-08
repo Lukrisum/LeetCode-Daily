b47d34ff2756c50ec438feed1d00a69cd9f398e4
Lukrisum <91905983+Lukrisum@users.noreply.github.com>
2022-12-08 23:39:44 +0800

    496. 下一个更大元素I

---
+++ 496.下一个更大元素-i.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=496 lang=cpp
+ *
+ * [496] 下一个更大元素 I
+ */
+
+// @lc code=start
+class Solution {
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> hashmap;
+        stack<int> st;
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            int num = nums2[i];
+            while (!st.empty() && num >= st.top()) {
+                st.pop();
+            }
+            hashmap[num] = st.empty() ? -1 : st.top();
+            st.push(num);
+        }
+        vector<int> res(nums1.size());
+        for (int i = 0; i < nums1.size(); ++i) {
+            res[i] = hashmap[nums1[i]];
+        }
+        return res;
+    }
+};
+
+
+// @lc code=end
+
